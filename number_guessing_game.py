535377e128ea627c3e0b856bf7853b7509754d3a
jmnieves12 <natureshadowlock723@gmail.com>
2024-02-12 18:04:55 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,27 +2,33 @@ import random
 num = random.randint(1, 100)
 
 user = 0
+upbound = 100
+lowbound = 1
 
-computerwins = 0
-userwins = 0
+guesses = 1
 
-computerwins = int(computerwins)
-userwins = int(userwins)
+response = 0
 
 user = input("Enter your guess: ")
 user = int(user)
 
 while user != num:
     if user > num:
-        print("Too high, it was", num)
-        input("Guess again: ")
+        print("Too high")
+        upbound = user
     elif user < num:
-        print("Too low, it was", num)
-        input("Guess again: ")
+        print("Too low")
+        lowbound = user
     else:
         print("You got it!")
-        if user == num:
-            break
+    user = input(f"Guess again from numbers {lowbound} to {upbound}: ")
+    user = int(user)
+    guesses += 1
+print(f"It took you {guesses} amount of guesses.")
+
+
+        # if user == num:
+            #break also skips the rest of the code
 
 #QUICK NOTE TO PROFESSOR REES:
         #My apologies for how incomplete this lab is. Since I missed class on the Wednesday that while loops were introduced and I had to do this on my own, I had a lot
